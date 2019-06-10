0ea9649982ff1e72d939fffd10948ce50929dcc4
Li Chong <delapecci@gmail.com>
2019-06-10 15:39:46 +0800

    Initial importing

---
+++ refresh_theme_dist.sh
@@ -0,0 +1,4 @@
+cp themes/even/static/dist/even.min.css public/dist/
+cp themes/even/static/dist/even.min.css.map public/dist/
+cp themes/even/static/dist/even.min.js public/dist/
+cp themes/even/static/dist/even.min.js.map public/dist/
